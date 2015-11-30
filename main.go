e86bcb2bf3c9256b5e7bfacde97cd0929322ae24
Carl Johnson <cmjohnson@pbs.org>
2015-11-30 10:48:50 -0500

    Don't follow redirects

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
+	netURL "net/url"
 	"os"
 	"sort"
 	"text/tabwriter"
@@ -40,10 +42,26 @@ func (h respHeader) String() string {
 	return buf.String()
 }
 
+// Sentinal error to let us know if we're ignoring a redirect
+var redirectError = errors.New("redirected")
+
+// Don't follow redirects
+func checkRedirect(req *http.Request, via []*http.Request) error {
+	return redirectError
+}
+
 func main() {
 	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
+		client := http.Client{
+			CheckRedirect: checkRedirect,
+		}
+		resp, err := client.Get(url)
+
+		// Ignore the error if it's just our redirectError
+		switch urlErr, ok := err.(*netURL.Error); {
+		case err == nil:
+		case ok && urlErr.Err == redirectError:
+		default:
 			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 			os.Exit(1)
 		}
