fc815c18ecfb7693671bcebcc9dc5aef526fedf7
Carl Johnson <cmjohnson@pbs.org>
2016-01-11 15:20:07 -0500

    Add timing and content length, plus gzip flag
    Too big for one commit, but oh well…

--- main.go
+++ main.go
@@ -5,23 +5,35 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	netURL "net/url"
 	"os"
+	"time"
 
 	"github.com/carlmjohnson/get-headers/prettyprint"
 )
 
 const usage = `Usage of get-headers:
 
-	get-headers <url>...
-		Gets the URLs and prints their headers alphabetically.
-		Repeated headers are printed with an asterisk.
-	get-headers (-h|--help)
-		Print this help message.
+get-headers [opts] <url>...
+       	Gets the URLs and prints their headers alphabetically.
+       	Repeated headers are printed with an asterisk.
+  -g	Shortcut for -gzip
+  -gzip
+       	Enable GZIP compression
+
+get-headers (-h|--help)
+	Print this help message.
 `
 
+var (
+	gzip = flag.Bool("gzip", false, "Enable GZIP compression")
+)
+
 func init() {
+	flag.BoolVar(gzip, "g", false, "Shortcut for -gzip")
 	flag.Usage = func() {
 		fmt.Fprint(os.Stderr, usage)
 	}
@@ -40,24 +52,50 @@ func checkRedirect(req *http.Request, via []*http.Request) error {
 	return errRedirect
 }
 
+func die(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "get-time: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
+	transport := &http.Transport{
+		DisableCompression: *gzip,
+	}
+	client := http.Client{
+		CheckRedirect: checkRedirect,
+		Transport:     transport,
+	}
 	for _, url := range flag.Args() {
-		client := http.Client{
-			CheckRedirect: checkRedirect,
-		}
+		var n int64
+		start := time.Now()
+
 		resp, err := client.Get(url)
 
 		// Ignore the error if it's just our errRedirect
 		switch urlErr, ok := err.(*netURL.Error); {
 		case err == nil:
+			// Copying to /dev/null just to make sure this is real
+			n, err = io.Copy(ioutil.Discard, resp.Body)
+			die(err)
 		case ok && urlErr.Err == errRedirect:
 		default:
-			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
-			os.Exit(1)
+			die(err)
 		}
-		resp.Body.Close()
+		duration := time.Since(start)
+		die(resp.Body.Close())
+
 		fmt.Println("GET", url)
 		fmt.Println(resp.Proto, resp.Status, "\n")
+		fmt.Println("Time           ", humanizeDuration(duration))
+		if n != 0 {
+			fmt.Println("Content length ", humanizeByteSize(n))
+			bps := int64(float64(n) / duration.Seconds())
+			fmt.Printf("Speed           %s/s\n", humanizeByteSize(bps))
+		}
+		fmt.Println()
 		fmt.Println(prettyprint.ResponseHeader(resp.Header))
+
 	}
 }
